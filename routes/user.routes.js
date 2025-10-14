6795079406036891f1d66c2741a7fa8bc3a600fe
Anujsi (Ext.Coforge) <anuj_singh@outlook.in>
2025-10-15 02:07:46 +0530

    added otp

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const { getMailTransporter } = require("../helpers/mail_transport");
 const userRouter = express.Router();
 
 const { user_email_verification } = require("../template/email-verify");
+const { forgot_password_otp_verification } = require("../template/otp/forgot-password-otp-verify");
 
 const mailTransportForUser = getMailTransporter(
   process.env.CLIENT_ID,
@@ -20,6 +21,27 @@ userRouter.post("/verify_email", (req, res) => {
 
   const htmlData = user_email_verification(name, url);
   mailOptions.subject = "Verify your email Adderss";
+  mailOptions.html = htmlData;
+
+  mailTransportForUser
+    .sendMail(mailOptions)
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false, error: err });
+    });
+});
+
+userRouter.post("/verify_otp", (req, res) => {
+  const { name, otp, email } = req.body;
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+  };
+
+  const htmlData = forgot_password_otp_verification(name, otp);
+  mailOptions.subject = "Forgot password from Shivi Kirana";
   mailOptions.html = htmlData;
 
   mailTransportForUser
